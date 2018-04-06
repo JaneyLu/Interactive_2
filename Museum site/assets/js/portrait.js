3c65c9d1d4675c82590d291a63a8eb57e03ad73e
JaneyLu <31681635+JaneyLu@users.noreply.github.com>
2018-04-05 19:34:35 -0700

    change

--- Museum site/assets/js/portrait.js
+++ Museum site/assets/js/portrait.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     
     $(".Araki").on({
-        mouseenter : function() {
-          $(".Araki").addClass(".active");
+        mouseenter : function () {
+          $(".Araki").addClass("active");
         },
-        mouseleave : function() {
-          $(".Araki").removeClass(".active");
+        mouseleave : function () {
+          $(".Araki").removeClass("active");
         }
     });  
     
