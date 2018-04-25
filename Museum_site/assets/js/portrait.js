cef07c39532ba007719a9cc2ae081c8c75145660
JaneyLu <31681635+JaneyLu@users.noreply.github.com>
2018-04-24 17:06:04 -0700

    fix positions

--- Museum_site/assets/js/portrait.js
+++ Museum_site/assets/js/portrait.js
@@ -6,25 +6,25 @@ $(document).ready(function () {
     $( "#draggable" ).draggable({ cursor: "move", cursorAt: { top: 150, left: 150 } 
     });
     
-    $( "#draggable_2" ).draggable({ cursor: "move", cursorAt: { top: 100, left: 90 } 
+    $( "#draggable_2" ).draggable({ cursor: "move", cursorAt: { top: 100, left: 430 } 
     });
     
-    $( "#draggable_3" ).draggable({ cursor: "move", cursorAt: { top: 200, left: 350 } 
+    $( "#draggable_3" ).draggable({ cursor: "move", cursorAt: { top: 100, left: 600 } 
     });
     
-    $( "#draggable_4" ).draggable({ cursor: "move", cursorAt: { top: 330, left: 290 } 
+    $( "#draggable_4" ).draggable({ cursor: "move", cursorAt: { top: 500, left: 240 } 
     });
     
-    $( "#draggable_5" ).draggable({ cursor: "move", cursorAt: { top: 150, left: 160 } 
+    $( "#draggable_5" ).draggable({ cursor: "move", cursorAt: { top: 450, left: 460 } 
     });
     
-    $( "#draggable_6" ).draggable({ cursor: "move", cursorAt: { top: 150, left: 160 } 
+    $( "#draggable_6" ).draggable({ cursor: "move", cursorAt: { top: 450, left: 600 } 
     });
     
-    $( "#draggable_7" ).draggable({ cursor: "move", cursorAt: { top: 150, left: 160 } 
+    $( "#draggable_7" ).draggable({ cursor: "move", cursorAt: { top: 600, left: 700 } 
     });
     
-    $( "#draggable_8" ).draggable({ cursor: "move", cursorAt: { top: 250, left: 460 } 
+    $( "#draggable_8" ).draggable({ cursor: "move", cursorAt: { top: 820, left: 170 } 
     });
     
     $(".nmark").click(function(){
