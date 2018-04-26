c8e03cbb61a95e2fec5385839b57b467aa0903e9
JaneyLu <31681635+JaneyLu@users.noreply.github.com>
2018-04-26 14:37:59 -0700

    content added and sticky header finished

--- Museum_site/assets/js/portrait.js
+++ Museum_site/assets/js/portrait.js
@@ -75,4 +75,96 @@ $(document).ready(function () {
         $("#content_3 .nmark").hide();
     });
     
+    $("#content_4 .nmark").click(function(){
+        $("#content_4").fadeOut(250);
+    });
+    
+    $("#dbl_4").dblclick(function(){
+        $("#content_4").fadeIn(500);
+    });
+    
+    $("#dbl_4").dblclick(function(){
+        $("#content_4 .nmark").show();
+    });
+    
+    $("#content_4 .nmark").click(function(){
+        $("#content_4 .nmark").hide();
+    });
+    
+    $("#content_5 .nmark").click(function(){
+        $("#content_5").fadeOut(250);
+    });
+    
+    $("#dbl_5").dblclick(function(){
+        $("#content_5").fadeIn(500);
+    });
+    
+    $("#dbl_5").dblclick(function(){
+        $("#content_5 .nmark").show();
+    });
+    
+    $("#content_5 .nmark").click(function(){
+        $("#content_5 .nmark").hide();
+    });
+    
+    $("#content_6 .nmark").click(function(){
+        $("#content_6").fadeOut(250);
+    });
+    
+    $("#dbl_6").dblclick(function(){
+        $("#content_6").fadeIn(500);
+    });
+    
+    $("#dbl_6").dblclick(function(){
+        $("#content_6 .nmark").show();
+    });
+    
+    $("#content_6 .nmark").click(function(){
+        $("#content_6 .nmark").hide();
+    });
+    
+    $("#content_7 .nmark").click(function(){
+        $("#content_7").fadeOut(250);
+    });
+    
+    $("#dbl_7").dblclick(function(){
+        $("#content_7").fadeIn(500);
+    });
+    
+    $("#dbl_7").dblclick(function(){
+        $("#content_7 .nmark").show();
+    });
+    
+    $("#content_7 .nmark").click(function(){
+        $("#content_7 .nmark").hide();
+    });
+    
+    $("#content_8 .nmark").click(function(){
+        $("#content_8").fadeOut(250);
+    });
+    
+    $("#dbl_8").dblclick(function(){
+        $("#content_8").fadeIn(500);
+    });
+    
+    $("#dbl_8").dblclick(function(){
+        $("#content_8 .nmark").show();
+    });
+    
+    $("#content_8 .nmark").click(function(){
+        $("#content_8 .nmark").hide();
+    });
+    
+    window.onscroll = function() {myFunction()};
+
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+    if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 });
